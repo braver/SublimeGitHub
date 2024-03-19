44cc4fc49918d4442085a853c7436598812fc08f
Koen Lageveen <braver@users.noreply.github.com>
2024-03-19 21:38:38 +0100

    reimplement based on SublimeGitUp

--- GitCommand.py
+++ GitCommand.py
@@ -3,23 +3,74 @@ import subprocess
 
 import sublime
 import sublime_plugin
- 
-
-class GithubCommand(sublime_plugin.WindowCommand):  
-    def run(self):  
-        filename = self.window.active_view().file_name()
-        dirname = os.path.dirname(filename)
-        while dirname != '/':
-            if '.git' in os.listdir(dirname):
-                return subprocess.call(['open', '-a', 'GitHub', dirname])
-            dirname = os.path.dirname(dirname)
-        subprocess.call(['open', '-a', 'GitHub'])
 
 
+def climb_dirs(start_dir):
+    right = True
+    while right:
+        yield start_dir
+        start_dir, right = os.path.split(start_dir)
 
 
+# look for the git root by traversing up the dir
+def find_git_root(path):
+    for folder in climb_dirs(path):
+        if os.path.exists(os.path.join(folder, '.git')):
+            return folder
 
 
+class GitHubOpenCommand(sublime_plugin.WindowCommand):
+
+    def is_enabled(self):
+        return True
+
+    def get_path(self):
+        filepath = self.window.active_view().file_name()
+        if filepath:
+            return find_git_root(os.path.dirname(filepath))
+
+        elif self.window.folders():
+            return find_git_root(self.window.folders()[0])
+
+        else:
+            sublime.status_message('No place to open GitUp to')
+            return False
+
+    def run(self, *args):
+        sublime.status_message('GitHub: running')
+        path = self.get_path()
+        if not path:
+            sublime.status_message('GitHub: No path')
+            return False
+        if os.path.isfile(path):
+            path = os.path.dirname(path)
+
+        app_path = '/Applications/GitHub Desktop.app'
+        subprocess.Popen(['open', '-a', app_path, path])
 
 
+class SideBarGitHubCommand(sublime_plugin.WindowCommand):
 
+    def is_enabled(self, paths):
+        for path in paths:
+            if find_git_root(path):
+                return True
+        return False
+
+    def get_path(self, paths):
+        try:
+            return find_git_root(paths[0])
+        except IndexError:
+            return self.window.active_view().file_name()
+
+    def run(self, paths):
+        sublime.status_message('GitHub: running')
+        path = self.get_path(paths)
+        if not path:
+            sublime.status_message('GitHub: No path')
+            return False
+        if os.path.isfile(path):
+            path = os.path.dirname(path)
+
+        app_path = '/Applications/GitHub Desktop.app'
+        subprocess.Popen(['open', '-a', app_path, path])
